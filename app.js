d781b6eab5debcbf70eac8ceb814908d527d4f44
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2017-12-11 11:25:14 -0800

    Completing the Event Handler

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-window.onload = function() {
-  alert("The window has loaded!");
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDeafault();
+
+    // get the text;
+    let title = newToDoText.value;
+
+    // create new li
+    let newLi = document.createElement('li');
+
+    // create new input;
+    let checkbox = documet.createElement('input');
+
+    // set the input type to checkbox
+    checkbox.type = "checkbox";
+
+    // set the title
+    newLi.textContent = title;
+
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+  });
 }
+
+
+window.onload = function() {
+  onReady();
+};
