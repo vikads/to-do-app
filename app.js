7418508dc6e35edf14b90933cb5b0fe4024b0526
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2017-12-12 13:49:39 -0800

    calling renderTheUi() from createNewToDo()

--- app.js
+++ app.js
@@ -5,40 +5,43 @@ function onReady() {
   const toDoList = document.getElementById('toDoList');
 
   function createNewToDo() {
+    if (!newToDoText.value) { return; }
+
     toDos.push({
       title: newToDoText.value,
       complete: false
     });
+
+    newToDoText.value = '';
+
+    renderTheUI();
   }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
 
-    toDos.forEach(function(toDo) {
-      const newToDo = document.createElement('li');
+    toDoList.textContent = '';
 
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
-      const.title = document.createElement('span');
-      title.textContent = toDo.title;
+      newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
     });
-
   }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
-    newToDoText.valuje = '';
+    newToDoText.value = '';
   });
 
   renderTheUI();
-
 };
-
 
 window.onload = function() {
   onReady();
