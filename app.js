29b0f3504593b367857f1e2e79a050ac36b424ed
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2017-12-12 12:10:22 -0800

    createNewToDo() function added, event listener to the form added

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+
+  function createNewToDo() {
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.valuje = '';
+  });
+    
+  }
 };
 
 
