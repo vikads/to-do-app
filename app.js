39c18289b5568cc0498e994baacc644d80fb45a1
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2017-12-12 12:41:03 -0800

    building the renderTheUI() function

--- app.js
+++ app.js
@@ -9,14 +9,34 @@ function onReady() {
       title: newToDoText.value,
       complete: false
     });
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDos.forEach(function(toDo) {
+      const newToDo = document.createElement('li');
+
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      const.title = document.createElement('span');
+      title.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+
+  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
     newToDoText.valuje = '';
   });
-    
-  }
+
+  renderTheUI();
+
 };
 
 
