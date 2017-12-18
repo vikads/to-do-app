d3eef812d58ff342ce87fa202e937d730bd233fa
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2017-12-17 23:01:25 -0800

    delete button added(using id), assignment 8 completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function onReady() {
+  let id = 0;
   let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
@@ -7,14 +8,23 @@ function onReady() {
   function createNewToDo() {
     if (!newToDoText.value) { return; }
 
+    console.log(id);
+
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++;
 
     newToDoText.value = '';
 
     renderTheUI();
+  }
+
+  function deleteToDo(id) {
+    toDos = toDos.filter(item => item.id !== id);
   }
 
   function renderTheUI() {
@@ -24,13 +34,26 @@ function onReady() {
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
+      newLi.id = toDo.id;
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+
+      let deleteButton = document.createElement('button');
+      deleteButton.type = "button";
+      deleteButton.innerHTML = "Delete to-do";
+      deleteButton.addEventListener('click', event => {
+        console.log('Delete Button Clicked: ', event);
+        deleteToDo(toDo.id);
+        renderTheUI();
+        
+      });
+
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
     });
   }
 
